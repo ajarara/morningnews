c55f87bb3d64f2005856e84fff745c3fba251e6b
Ahmad Jarara <ajarara@jarmac.org>
2017-05-19 17:03:44 -0400

    Make auth internal

--- morningnews.py
+++ morningnews.py
@@ -2,6 +2,7 @@ import json
 import requests
 from hashlib import md5, sha1
 
+from cachecontrol import CacheControl
 
 EMBY_AUTH = 'MediaBrowser Client="Emby Mobile", Device="Firefox", DeviceId="27d9f5adc15f875265f6aeb29062218d7a58c717", Version="3.2.10.0"'
 
@@ -16,76 +17,30 @@ class EmbyController():
         self.password = password.encode('utf-8')  # for hashlib
         self.emby_ip = emby_ip
         self.emby_token_url = "http://{}/emby/Users/authenticatebyname".format(emby_ip)
-        self.params = json.dumps(
+        self._auth_params = json.dumps(
             {
                 'Password': sha1(self.password).hexdigest(),
                 'PasswordMd5': md5(self.password).hexdigest(),
                 'Username': self.username
             }
         )
-        self.headers = {
+        self._auth_headers = {
             'Content-type': 'application/json',
             'x-emby-authorization': auth,
-            'Content-Length': '{}'.format(len(self.params))
+            'Content-Length': '{}'.format(len(self._auth_params))
             }
-        # this will be mutated once we've authed with the token
-        self.auth_headers = None
-        # this will be mutated once we've hit Emby's api
-        self.resp = None
-        # likewise as above
-        self.token = None
-        # eugh
-        self.props = None
-        self.library = None
+        self.sess = CacheControl(requests.session())
+        self.api_key = None
+        self.id = None
         
-    def authenticate(self):
-        resp = requests.post(self.emby_token_url,
-                             data=self.params,
-                             headers = self.headers)
-        if resp.status_code != 200:
-            print('''
-            Err: Emby no auth. Username: {}
-            sha1: {}
-            Headers: {}
-            Content: {}'''.format(self.username,
-                                  sha1(self.password).hexdigest(),
-                                  resp.headers,
-                                  resp.content),
-                  Flush=True)
-        self.resp = resp
-        return resp
+        
+    def _get_api(self):
+        resp =  self.sess.post(
+            self.emby_token_url,
+            data = self._auth_params,
+            headers = self._auth_headers)
+        if resp.status != 200:
+            raise ValueError()
+        return json.loads(resp.content.decode('utf-8'))
 
-    def jsonify_resp(self):
-        if not self.resp:
-            self.authenticate()
-        if not self.props:
-            self.props = json.loads(self.resp.content.decode('utf-8'))
-        return self.props
-
-    def get_token(self):
-        if not self.token:
-            self.token = self.jsonify_resp()['AccessToken']
-        self.auth_headers = self.headers.copy().update(
-            {'X-MediaBrowser-Token': self.token})
-        return self.token
-
-    # this indentation plays mind tricks. Don't trust it.
-    def get_library(self):
-        if not self.library:
-            self.library = requests.get(
-                "http://{}{}".format(
-                    self.emby_ip,
-                    "/Users/{}/Views".format(
-                        self.jsonify_resp()['SessionInfo']['UserId'])),
-                headers=self.auth_headers)
-        return self.library
-            
-
-    def get_live_tv(self):
-        return requests.get(
-            "http://{}{}".format(
-                self.emby_ip,
-                "/Users/{}/Items?parentId={}".format(
-                    self.jsonify_resp()['SessionInfo']['UserId'],
-                    self.get_library.content['Items'])),
-            headers=self.auth_headers)
+    
