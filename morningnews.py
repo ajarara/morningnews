5d7db40c1aec202f865ffd013e216783e8322b61
Ahmad Jarara <ajarara@jarmac.org>
2017-05-22 14:03:24 -0400

    Add get_live_channels method

--- morningnews.py
+++ morningnews.py
@@ -2,9 +2,11 @@ import json
 import requests
 from hashlib import md5, sha1
 
-from contextlib import contextmanager
+# ==================== BEGIN GLOBALS ====================
+# later on we will parse these as runtime options.
 
-# this is more or less arbitrary, but it will effect the way it's represented in the UI
+# this is more or less arbitrary, but it will effect the way it's
+# represented in the UI
 DEVICE = 'MediaBrowser Client="Emby Mobile", Device="Firefox", DeviceId="27d9f5adc15f875265f6aeb29062218d7a58c717", Version="3.2.10.0"'
 
 EMBY_IP = '192.168.5.151:8096'
@@ -13,19 +15,135 @@ PROTO = 'http://'
 
 EMBY_URL = PROTO + EMBY_IP
 
+# ==================== END GLOBALS ====================
+
+def sess(_sess=[]):
+    if _sess:
+        return _sess[0]
+    _sess.append(requests.session())
+    return _sess[0]
+    
+
+def getAuthRespFor(username, password, _cache={}, device=DEVICE):
+    ''' EMBY ONLY '''
+    password = password.encode('utf-8')
+    if (username, password) in _cache:
+        return _cache[username, password]
+    emby_token_url = "{}/emby/Users/authenticatebyname".format(EMBY_URL)
+    auth_params = json.dumps(
+        {
+            'Password': sha1(password).hexdigest(),
+            'PasswordMd5': md5(password).hexdigest(),
+            'Username': username
+        }
+    )
+    headers = {
+        'Content-type': 'application/json',
+        # don't be fooled by this, this is just to tell emby what kind
+        # of device you've got. Not important, but mandatory.
+        'x-emby-authorization': device
+    }
+    resp = sess().post(
+        emby_token_url,
+        data = auth_params,
+        headers = headers)
+    _emby_conn_check((username, password), resp, "Emby auth failed!")
+    _cache[username, password] = resp
+    return resp
+
+def _authHeaders(accessToken):
+    return {
+        'Content-type': 'application/json',
+        'X-MediaBrowser-Token': accessToken
+        }
+    
+
+def get_prop_from_resp(resp, prop):
+    # whether or not this should be pushed up
+    # is an interesting design choice
+    content = resp_to_content_dict(resp)
+    # if there's a valueError we want to know about it, so don't catch them.
+
+    # one of the issues is that possibly not all responses have these.
+    return {
+        'UserId': content['SessionInfo']['UserId'],
+        'AccessToken': content['AccessToken']
+        }[prop]
+
+def getLiveTv(key, UserId):
+    return sess().get(
+        "{}/emby/Users/{}/Views".format(
+            EMBY_URL,
+            UserId),
+        headers = _authHeaders(key))
+
+
+
+# ====================  CLASS BASED ====================
+
+def resp_to_content_dict(resp):
+    return json.loads(resp.content.decode('utf-8'))
+
+def _extract_invariants_from_resp(resp):
+    vals = resp_to_content_dict(resp)
+    return {
+        'UserId': vals['SessionInfo']['UserId'],
+        'AccessToken': vals['AccessToken']
+    }
+
 
 class EmbyController():
 
-    def __init__(self, username, password, emby_ip=EMBY_IP):
+    def __init__(self, username, password, emby_ip=EMBY_IP,
+                 proto=PROTO, device=DEVICE):
         self.username = username
         self.password = password.encode('utf-8')  # for hashlib
         self.emby_ip = emby_ip
-        self.emby_url = PROTO + emby_ip
-        self._api = None
+        self.device = DEVICE
 
-    def _get_api(self, auth=DEVICE):
-        ''' Get the full http response from an Auth attempt. A 200
-        indicates success, anything else indicates otherwise.'''
+        self.emby_url = proto + emby_ip
+        self.session_state = None
+
+    def get(self, directory, auth=False):
+        ''' get a http response from server '''
+        if auth:
+            resp = requests.get(
+                '{}'.format(self.emby_url) + directory,
+                headers = self._auth_headers()
+            )
+        else:
+            resp = requests.get(
+                '{}'.format(self.emby_url) + directory)
+        self._emby_conn_check(resp, "Get request failed!")
+        return resp
+
+    def post(self, directory, auth=False):
+        ''' post a http form to server, returning reply '''
+        if auth:
+            resp = requests.post(
+                '{}'.format(self.emby_url) + directory,
+                headers = self._auth_headers()
+                )
+        else:
+            resp = requests.post(
+                '{}'.format(self.emby_url) + directory)
+        self._emby_conn_check(resp, "Post request failed!")
+        return resp
+
+    def session_invariants(self, key=None):
+        ''' 
+        Interface to all session invariants, including api
+        key. Can be used as a generic interface
+        '''
+        # we want userid, and api key for now, but want to have the ability
+        # to expand on this without breaking the interface.
+        # likely a dictionary is best.
+        if self.session_state:
+            if key:
+                return self.session_state[key]
+            return self.session_state
+        # okay, if at this point it means 
+        # we haven't authorized. Let's do so.
         emby_token_url = "{}/emby/Users/authenticatebyname".format(self.emby_url)
         # encoding?
         auth_params = json.dumps(
@@ -37,83 +155,77 @@ class EmbyController():
         )
         auth_headers = {
             'Content-type': 'application/json',
-            'x-emby-authorization': auth,
+            'x-emby-authorization': self.device,
         }
         resp =  requests.post(
             emby_token_url,
             data = auth_params,
             headers = auth_headers)
-        _emby_conn_check(self, resp, "Emby auth failed!")
-        return resp
+        self._emby_conn_check(resp, "Emby auth failed!")
+        self.session_state = _extract_invariants_from_resp(resp)
 
+        # this code is duplicated above, but it's three lines or so
+        if key:
+            return self.session_state[key]
+        return self.session_state
+
+    # ==================== UTILITY ====================
+    def get_views(self, collectionId=''):
+        ''' Flags are not checked for validity. They're simply
+        appended to the end of the url string.'''
+        if not collectionId:
+            url = "/Users/{}/Views".format(self.session_invariants('UserId'))
+        else:
+            url = "/Users/{}/Items?parentId={}".format(
+                self.session_invariants('UserId'),
+                collectionId)
+            
+        resp = self.get(url, auth=True)
+        return resp_to_content_dict(resp)
+
+    def get_live_channels(self):
+        # we aren't interested in record count.
+        top_level_views = self.get_views()['Items']
+        # get the first livetv collection (there is only one)
+        livetv_id = self._get_first_colltype(top_level_views, 'livetv')['Id']
+
+        # man this is a verbose API
+        livetv_coll = self.get_views(livetv_id)['Items']
+        livetv_channels_id = self._get_first_colltype(
+            livetv_coll, 'LiveTvChannels')['Id']
+        return self.get_views(livetv_channels_id)['Items']
+
+
+    def get_channel_id(self, channel_name):
+        pass
+
+
+    # ==================== GUTS ====================
+
+    def _get_first_colltype(self, coll, colltype):
+        return next(x for x in coll if x['CollectionType'] == colltype)
+    
     def _auth_headers(self):
         # do we make posts exclusively with json? should that be here?
         return {
             'Content-type': 'application/json',
-            'X-MediaBrowser-Token': self.api()['AccessToken']
+            'X-MediaBrowser-Token': self.session_invariants(key='AccessToken')
         }
 
+    def _emby_conn_check(self, conn, error_msg):
+        if conn.status_code != 200:
+            raise ValueError('''
+            Err: {} 
+            Username: {}
+            sha1: {}
+            md5: {}
+            Headers: {}
+            Content: {}'''.format(
+                error_msg,
+                self.username,
+                sha1(self.password).hexdigest(),
+                md5(self.password).hexdigest(),
+                conn.headers,
+                conn.content))
 
-    def api(self, _cache={}):
-        ''' 
-        Return an easily digestible interface to the... interface
-        '''
-        if not self._api:
-            sess = self._get_api()
-            self._api = _json_to_dict(sess.content)
-
-        # a interesting sanity check is to see if _cache has different
-        # keys inside it than what we expect. If so, throw an exception
-        # as the _cache is being overriden by something. This won't
-        # catch all cases but it's interesting enough to make note of.
-
-        # if the cache isn't populated, make it so
-        if not _cache:
-            _cache['UserId'] = self._api['SessionInfo']['UserId']
-            _cache['AccessToken'] = self._api['AccessToken']
-        return _cache
-
-    def get_library(self):
-        emby_lib_url = "{}/emby/Users/{}/Views".format(
-            self.emby_url,
-            self.api()['UserId']
-        )
-        conn = requests.get(emby_lib_url, headers=self._auth_headers())
-        _emby_conn_check(self, conn, "Emby library retrieve failed!")
-        return _json_to_dict(conn.content)
-
-    def get_live_tv(self):
-        emby_live_tv = "{}/emby/LiveTv/Channels".format(
-            self.emby_url
-        )
-        conn = requests.get(emby_live_tv, headers=self._auth_headers())
-        _emby_conn_check(self, conn, "Emby live tv retrieve failed!")
-        return _json_to_dict(conn.content)
-
-    # not sure if this is necessary. It returns a 401 in the web client.
-    # def logout(self):
-    #     emby_logout_url = "{}/emby/Sessions/Logout".format(self.emby_url)
-    #     resp = requests.post(emby_logout_url, headers=self._auth_headers())
-    #     _emby_conn_check(self, resp, "Emby logout failed!")
-    #     return True
-
-
-def _json_to_dict(jsonbytes):
-    return json.loads(jsonbytes.decode('utf-8'))
-
-def _emby_conn_check(controller, conn, error_msg):
-    if conn.status_code != 200:
-        raise ValueError('''
-        Err: {} 
-        Username: {}
-        sha1: {}
-        md5: {}
-        Headers: {}
-        Content: {}'''.format(
-            error_msg,
-            controller.username,
-            sha1(controller.password).hexdigest(),
-            md5(controller.password).hexdigest(),
-            conn.headers,
-            conn.content))
 
